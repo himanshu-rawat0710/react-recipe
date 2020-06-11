1857ea814c85d4486991620ebf4887fe0cb8eab1
HMN5HU <rawath435@gmail.com>
2020-06-11 21:27:51 +0530

    changing navbar by bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <h3>hello from home.js</h3>
-            </div>
+            <Header />
         )
     }
 }
