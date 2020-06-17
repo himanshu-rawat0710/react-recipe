714154eeb3758ac7a0b671c40a62b760c54549ad
HMN5HU <rawath435@gmail.com>
2020-06-17 23:08:33 +0530

    changed toggling of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,28 +6,35 @@ import {Link} from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-md bg-light navbar-light">
+        <nav className="navbar navbar-expand-md bg-light navbar-light" role='navigation'>
         {/* nav-brand  */}
         <Link to='/' className='navbar-brand'>
                 <img src={logo} alt='logo' />
         </Link>
       
         {/* <!-- Toggler/collapsibe Button --> */}
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
       
         {/* <!-- Navbar links --> */}
-        <div className="collapse navbar-collapse show ml-sm-5" id="collapsibleNavbar">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link className='nav-link' to='/'>
+        <div className="collapse navbar-collapse ml-sm-5" id="collapsibleNavbar">
+                <ul className="nav navbar-nav mr-auto">
+                    <li className="nav-item" data-toggle="collapse" data-target="#collapsibleNavbar">
+                        <Link className='nav-link d-none d-sm-none d-md-block' to='/'>
+                            Home
+                        </Link>
+                        
+                        <Link to='/' className='nav-link d-block d-sm-block d-md-none'   >
                             Home
                         </Link>
                     </li>
 
-                    <li className="nav-item">
-                        <Link className='nav-link' to='/recipes'>
+                    <li className="nav-item" data-toggle="collapse" data-target="#collapsibleNavbar">
+                        <Link className='nav-link d-none d-sm-none d-md-block' to='/recipes'>
+                            Recipes
+                        </Link>
+                        <Link className='nav-link d-block d-sm-block d-md-none' to='/recipes'>
                             Recipes
                         </Link>
                     </li>
