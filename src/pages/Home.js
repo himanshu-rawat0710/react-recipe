c36527dd7458dc0589899bbd86a8f8673e25167e
HMN5HU <rawath435@gmail.com>
2020-06-19 20:55:50 +0530

    testing style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 export default class Home extends Component {
     render() {
         return (
-            <Header className = 'header-hero' title='Tasty Recipes' style={{color:'white', fontSize:'50px'}}>
+            <Header className = 'header-hero' title='Tasty Recipes' style={{color:'white', fontSize:'50px !important'}}>
                 <h1 className='text-uppercase text-slanted' style={{color:'white', fontSize:'40px'}}>with</h1>
                 <h2 className='display-3 text-uppercase my-5' style={{color:'white', fontWeight:'bold'}}>Food<strong className='text-orange'>gasm</strong></h2>
             <Link to='recipes' className='text-uppercase btn btn-secondary btn-lg mt-3'>
